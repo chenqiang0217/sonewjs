31cfcc0899495a3f43c9b21b2e11e08dbc48639f
chenqiang0217 <419788322@qq.com>
2024-06-05 18:39:40 +0800

    '...'

--- src/components/bottom/Server.vue
+++ src/components/bottom/Server.vue
@@ -4,10 +4,10 @@ import { Message } from '../../api/message'
 
 const messages = useMessageStore()
 const icons = [
-    {level: Message.LEVEL.INFO, name: 'about'},
-    {level: Message.LEVEL.SUCCESS, name: 'sucess'},
-    {level: Message.LEVEL.WARNING, name: 'caution'},
-    {level: Message.LEVEL.ERROR, name: 'error'}
+    { level: Message.LEVEL.INFO, type: 'info', name: 'about', color: '#909399' },
+    { level: Message.LEVEL.SUCCESS, type: 'success', name: 'sucess', color: '#67C23A' },
+    { level: Message.LEVEL.WARNING, type: 'warning', name: 'caution', color: '#E6A23C' },
+    { level: Message.LEVEL.ERROR, type: 'danger', name: 'error', color: '#F56C6C' }
 ]
 </script>
 
@@ -15,8 +15,11 @@ const icons = [
     <el-scrollbar>
         <div v-for="message in messages.server">
             <div>
-                <IconFront :iconName="icons.find(item => item.level == message.level).name" color="black" size="small"></IconFront>
-                {{ message.text }}
+                <el-text :type="icons.find(item => item.level == message.level).type">
+                    <IconFront :iconName="icons.find(item => item.level == message.level).name"
+                        :color="icons.find(item => item.level == message.level).color" size="small"></IconFront>
+                    {{ message.text }}
+                </el-text>
             </div>
         </div>
     </el-scrollbar>
@@ -25,5 +28,13 @@ const icons = [
 <style scoped>
 div {
     font-size: 14px;
+}
+
+.info {
+    color: rgb(51, 49, 49);
+}
+
+.su {
+    color: rgb(51, 49, 49);
 }
 </style>
